4f4f9b079f1efda1d808a0a80691b51533663089
eis <eis@iki.fi>
2014-11-04 22:20:16 +0200

    initial start screen

--- src/main/java/fi/eis/applications/chatapp/LoginUI.java
+++ src/main/java/fi/eis/applications/chatapp/LoginUI.java
@@ -6,11 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Blackstorm
+ * User: eis
  * Date: 4.11.2014
  * Time: 20:16
- * To change this template use File | Settings | File Templates.
  */
 public class LoginUI extends JFrame {
 
@@ -40,9 +38,56 @@ public class LoginUI extends JFrame {
 
     private JPanel createLoginPanel() {
         JPanel panel = new JPanel(new BorderLayout());
+        //panel.setPreferredSize(new Dimension(500, 500));
+        panel.add(createLoginHeader(), BorderLayout.NORTH);
+        panel.add(createRoomSelector(), BorderLayout.CENTER);
+        panel.add(createLoginFooter(), BorderLayout.SOUTH);
+        return panel;
+    }
+
+    private JPanel createLoginFooter() {
+        JPanel panel = new JPanel(new BorderLayout());
         loginButton = new JButton("Login");
         panel.add(loginButton);
         return panel;
     }
 
+    private static Component createRoomSelector() {
+        JScrollPane roomListScrollPane;
+        {
+            DefaultListModel listModel = new DefaultListModel();
+            listModel.addElement("Seurahuone");
+            listModel.addElement("Hautausmaa");
+            listModel.addElement("Helsinki");
+
+            JList list = new JList(listModel);
+            list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            list.setSelectedIndex(0);
+            roomListScrollPane = new JScrollPane(list);
+        }
+
+        JScrollPane roomMemberListScrollPane;
+        {
+            DefaultListModel listModel = new DefaultListModel();
+            listModel.addElement("user1");
+            listModel.addElement("user2");
+            listModel.addElement("user3");
+
+            JList list = new JList(listModel);
+            list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            list.setSelectedIndex(0);
+            roomMemberListScrollPane = new JScrollPane(list);
+        }
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(roomListScrollPane, BorderLayout.WEST);
+        panel.add(roomMemberListScrollPane, BorderLayout.EAST);
+        return panel;
+    }
+
+
+    private JPanel createLoginHeader() {
+        JPanel panel = new JPanel();
+        return panel;
+    }
 }
