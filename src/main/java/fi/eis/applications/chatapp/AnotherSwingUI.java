bf687fe563626194d67acd4aebf62153a02f7356
eis <eis@iki.fi>
2014-11-04 20:01:45 +0200

    now with option to use user list

--- src/main/java/fi/eis/applications/chatapp/AnotherSwingUI.java
+++ src/main/java/fi/eis/applications/chatapp/AnotherSwingUI.java
@@ -1,101 +1,202 @@
 package fi.eis.applications.chatapp;
 
 import javax.swing.*;
-
-import java.awt.*;              //for layout managers and more
-import java.awt.event.*;        //for action events
-
-import java.io.IOException;
+import java.awt.*;
+import java.awt.event.*;
 
 /**
  * Created with IntelliJ IDEA.
- * User: Blackstorm
+ * User: eis
  * Date: 3.11.2014
  * Time: 22:53
- * To change this template use File | Settings | File Templates.
  */
-public class AnotherSwingUI extends JPanel
+public class AnotherSwingUI extends JFrame
         implements ActionListener {
 
-        private String newline = "\n";
-        private JTextField inputField;
+    private JTextField inputField;
+    private JPanel userListPanel;
+    private JPanel messagesPanel;
+    private JPanel inputPanel;
 
-        public JTextField getInputField() {
-            return inputField;
+    public AnotherSwingUI() {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setPreferredSize(new Dimension(500, 500));
+
+        userListPanel = createUserListPanel();
+
+        messagesPanel = createMessagesPanel();
+
+        inputPanel = createInputPanel();
+
+        JMenuBar menuBar = createMenu();
+
+        // Put everything on a panel.
+        panel.add(messagesPanel,
+                BorderLayout.CENTER);
+        panel.add(userListPanel,
+                BorderLayout.EAST);
+        panel.add(inputPanel,
+                BorderLayout.PAGE_END);
+
+        // JFrame operations
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setJMenuBar(menuBar);
+        add(panel);
+    }
+
+    /**
+     * Returns an ImageIcon, or null if the path was invalid.
+     */
+    protected static ImageIcon createImageIcon(String path,
+                                               String description) {
+        java.net.URL imgURL = AnotherSwingUI.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
         }
+    }
 
-        public AnotherSwingUI() {
-            setLayout(new BorderLayout());
-            setPreferredSize(new Dimension(500,500));
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event dispatch thread.
+     */
+    public static void createAndShowGUI() {
+        //Create and set up the window.
+        AnotherSwingUI frame = new AnotherSwingUI();
 
-            //Create a regular text field.
-            inputField = new JTextField(10);
-            inputField.addActionListener(this);
+        // pack makes size correspond to content
+        frame.pack();
+        // Center the window
+        frame.setLocationRelativeTo(null);
+        // Set focus
+        frame.focusOnInput();
 
-            //Lay out the text controls and the labels.
-            JPanel textControlsPane = new JPanel();
-            GridBagLayout gridbag = new GridBagLayout();
-            GridBagConstraints c = new GridBagConstraints();
+        // Show
+        frame.setVisible(true);
+    }
 
-            textControlsPane.setLayout(gridbag);
+    private void focusOnInput() {
+        getInputField().requestFocusInWindow();
+    }
 
-            JTextField[] textFields = {inputField};
-            addLabelTextRows(textFields, textControlsPane);
+    private JMenuBar createMenu() {
+        //Create the menu bar.
+        JMenuBar menuBar = new JMenuBar();
 
-            c.gridwidth = GridBagConstraints.REMAINDER; //last
-            c.anchor = GridBagConstraints.WEST;
-            c.weightx = 1.0;
+        //Build the first menu.
+        JMenu menu = new JMenu("Window");
+        menu.setMnemonic(KeyEvent.VK_W);
 
-            textControlsPane.setBorder(
-                    BorderFactory.createCompoundBorder(
-                            BorderFactory.createTitledBorder("Input"),
-                            BorderFactory.createEmptyBorder(5,5,5,5)));
+        JCheckBoxMenuItem cbMenuItem = new JCheckBoxMenuItem("User list");
 
-            //Create an editor pane.
-            JEditorPane editorPane = createEditorPane();
-            JScrollPane editorScrollPane = new JScrollPane(editorPane);
-            editorScrollPane.setVerticalScrollBarPolicy(
-                    JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-            editorScrollPane.setHorizontalScrollBarPolicy(
-                    JScrollPane.HORIZONTAL_SCROLLBAR_NEVER
-            );
-            editorScrollPane.setPreferredSize(new Dimension(250, 145));
-            editorScrollPane.setMinimumSize(new Dimension(10, 10));
-
-
-            JPanel chatPane = new JPanel(new GridLayout(1,0));
-            chatPane.add(editorScrollPane);
-            chatPane.setBorder(BorderFactory.createCompoundBorder(
-                    BorderFactory.createTitledBorder("Messages"),
-                    BorderFactory.createEmptyBorder(5, 5, 5, 5)));
-
-
-            //Put everything together.
-            JPanel leftPane = new JPanel(new BorderLayout());
-            leftPane.add(chatPane,
-                    BorderLayout.CENTER);
-            leftPane.add(textControlsPane,
-                    BorderLayout.PAGE_END);
-
-            add(leftPane, BorderLayout.CENTER);
-
-        }
-
-        private void addLabelTextRows(JTextField[] textFields,
-                                      Container container) {
-            GridBagConstraints c = new GridBagConstraints();
-            c.anchor = GridBagConstraints.EAST;
-            int numLabels = textFields.length;
-
-            for (int i = 0; i < numLabels; i++) {
-                c.gridwidth = GridBagConstraints.REMAINDER;     //end row
-                c.fill = GridBagConstraints.HORIZONTAL;
-                c.weightx = 1.0;
-                container.add(textFields[i], c);
+        cbMenuItem.setState(true);
+        cbMenuItem.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                userListPanel.setVisible(e.getStateChange() == ItemEvent.SELECTED);
             }
-        }
+        });
 
-        public void actionPerformed(ActionEvent e) {
+        menu.add(cbMenuItem);
+        menuBar.add(menu);
+
+        return menuBar;
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event dispatching thread:
+        //creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                //Turn off metal's use of bold fonts
+                UIManager.put("swing.boldMetal", Boolean.FALSE);
+                createAndShowGUI();
+            }
+        });
+    }
+
+    public JTextField getInputField() {
+        return inputField;
+    }
+
+    private JPanel createInputPanel() {
+        inputField = new JTextField(10);
+        inputField.addActionListener(this);
+
+        JPanel inputPanel = new JPanel(new GridBagLayout());
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        JTextField[] textFields = {inputField};
+        addLabelTextRows(textFields, inputPanel);
+
+        c.gridwidth = GridBagConstraints.REMAINDER; //last
+        c.anchor = GridBagConstraints.WEST;
+        c.weightx = 1.0;
+
+        inputPanel.setBorder(
+                BorderFactory.createCompoundBorder(
+                        BorderFactory.createTitledBorder("Input"),
+                        BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+        return inputPanel;
+    }
+
+    private JPanel createUserListPanel() {
+        JEditorPane userListPane = createEditorPane();
+        JScrollPane userListScrollPane = new JScrollPane(userListPane);
+        userListScrollPane.setVerticalScrollBarPolicy(
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        userListScrollPane.setHorizontalScrollBarPolicy(
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER
+        );
+        userListScrollPane.setMinimumSize(new Dimension(10, 10));
+
+
+        JPanel userListPanel = new JPanel(new GridLayout(1, 0));
+        userListPanel.add(userListScrollPane);
+        userListPanel.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createTitledBorder("User list"),
+                BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+        return userListPanel;
+    }
+
+    private JPanel createMessagesPanel() {
+        JEditorPane editorPane = createEditorPane();
+        JScrollPane editorScrollPane = new JScrollPane(editorPane);
+        editorScrollPane.setVerticalScrollBarPolicy(
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        editorScrollPane.setHorizontalScrollBarPolicy(
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER
+        );
+        editorScrollPane.setMinimumSize(new Dimension(10, 10));
+
+
+        JPanel chatPane = new JPanel(new GridLayout(1, 0));
+        chatPane.add(editorScrollPane);
+        chatPane.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createTitledBorder("Messages"),
+                BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+        return chatPane;
+    }
+
+    private void addLabelTextRows(JTextField[] textFields,
+                                  Container container) {
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.EAST;
+        int numLabels = textFields.length;
+
+        for (int i = 0; i < numLabels; i++) {
+            c.gridwidth = GridBagConstraints.REMAINDER;     //end row
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.weightx = 1.0;
+            container.add(textFields[i], c);
+        }
+    }
+
+    public void actionPerformed(ActionEvent e) {
             /*
             String prefix = "You typed \"";
             if (textFieldString.equals(e.getActionCommand())) {
@@ -109,71 +210,26 @@ public class AnotherSwingUI extends JPanel
                 Toolkit.getDefaultToolkit().beep();
             }
             */
-        }
-        private JEditorPane createEditorPane() {
-            JEditorPane editorPane = new JEditorPane();
-            editorPane.setEditable(false);
-            java.net.URL helpURL = this.getClass().getResource(
-                    "TextSamplerDemoHelp.html");
-            if (helpURL != null) {
-                try {
-                    editorPane.setPage(helpURL);
-                } catch (IOException e) {
-                    System.err.println("Attempted to read a bad URL: " + helpURL);
-                }
-            } else {
-                System.err.println("Couldn't find file: TextSampleDemoHelp.html");
+    }
+
+    private JEditorPane createEditorPane() {
+        JEditorPane editorPane = new JEditorPane();
+        editorPane.setEditable(false);
+        /*
+        java.net.URL helpURL = this.getClass().getResource(
+                "TextSamplerDemoHelp.html");
+        if (helpURL != null) {
+            try {
+                editorPane.setPage(helpURL);
+            } catch (IOException e) {
+                System.err.println("Attempted to read a bad URL: " + helpURL);
             }
-
-            return editorPane;
+        } else {
+            System.err.println("Couldn't find file: TextSampleDemoHelp.html");
         }
+        */
 
-        /** Returns an ImageIcon, or null if the path was invalid. */
-        protected static ImageIcon createImageIcon(String path,
-                                                   String description) {
-            java.net.URL imgURL = AnotherSwingUI.class.getResource(path);
-            if (imgURL != null) {
-                return new ImageIcon(imgURL, description);
-            } else {
-                System.err.println("Couldn't find file: " + path);
-                return null;
-            }
-        }
-
-        /**
-         * Create the GUI and show it.  For thread safety,
-         * this method should be invoked from the
-         * event dispatch thread.
-         */
-        public static void createAndShowGUI() {
-            //Create and set up the window.
-            JFrame frame = new JFrame("Chat");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            //Add content to the window.
-            AnotherSwingUI panel = new AnotherSwingUI();
-            frame.add(panel);
-
-            // pack makes size correspond to content
-            frame.pack();
-            // Center the window
-            frame.setLocationRelativeTo(null);
-            // default focus
-            panel.getInputField().requestFocusInWindow();
-            // Show
-            frame.setVisible(true);
-        }
-
-        public static void main(String[] args) {
-            //Schedule a job for the event dispatching thread:
-            //creating and showing this application's GUI.
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                    //Turn off metal's use of bold fonts
-                    UIManager.put("swing.boldMetal", Boolean.FALSE);
-                    createAndShowGUI();
-                }
-            });
-        }
+        return editorPane;
+    }
 
 }
